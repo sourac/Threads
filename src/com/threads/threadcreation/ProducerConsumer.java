306acd7be4ed59312f6dd6c469c7d8e07488acac
Atul Sharma <atulsharma21772@gmail.com>
2020-06-23 13:30:39 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

---
+++ src/com/threads/threadcreation/ProducerConsumer.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.threads.threadcreation;
+
+/**
+ * @author Atul Sharma
+ *
+ *         https://github.com/sourac
+ */
+class PNC {
+
+}
+
+public class ProducerConsumer {
+
+	public static void main(String[] args) {
+		
+		PNC pnc=new PNC();
+
+		Thread t1 = new Thread(new Runnable() {
+
+
+			@Override
+			public void run() {
+
+			}
+		});
+
+	}
+
+}
