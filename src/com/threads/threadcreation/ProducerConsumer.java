2dde24b750c6f8df9a194bb761814a2ff2e2e5fb
Atul Sharma <atulsharma21772@gmail.com>
2020-06-23 13:32:42 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/threads/threadcreation/ProducerConsumer.java
+++ src/com/threads/threadcreation/ProducerConsumer.java
@@ -15,17 +15,28 @@ class PNC {
 public class ProducerConsumer {
 
 	public static void main(String[] args) {
-		
-		PNC pnc=new PNC();
+
+		PNC pnc = new PNC();
 
 		Thread t1 = new Thread(new Runnable() {
 
-
 			@Override
 			public void run() {
-
+				pnc.producer();
 			}
 		});
+		
+		Thread t2=new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				pnc.consumer();
+				
+			}
+		});
+		
+		t1.start();
+		t2.start();
 
 	}
 
